e7cf382f872b9255d19bfb8230051619a5c6a8c7
Cristian Gonzalez <cristian3629@gmail.com>
2020-10-26 20:14:20 -0300

    add sleep

--- src/reverse_ping_srv.py
+++ src/reverse_ping_srv.py
@@ -1,9 +1,13 @@
 from socket_client import send_ping, send_msg
+import time
+
+time_to_wait = 1
 
 
 def reverse_ping_srv(socket_client, counts):
     print("reverse proxy request received counts -> {}".format(counts))
     for counts in range(counts):
+        time.sleep(time_to_wait)
         measure_in_ms = send_ping(socket_client)
         send_msg(socket_client, str(measure_in_ms))
     print("reverse ping finished")
