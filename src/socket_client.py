3099964baa6a8c647cecdd165cca58b2f22a38c8
alfo777 <alfonso71292@gmail.com>
2020-10-28 23:16:47 -0300

    add timeout

--- src/socket_client.py
+++ src/socket_client.py
@@ -7,6 +7,7 @@ import socket
 def create_socket(host, port):
     server_address = (host, port)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.timeout(TIMEOUT)
     try:
         sock.connect(server_address)
 
