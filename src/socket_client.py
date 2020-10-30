f84f82614f3cc74e344e325b75e494a467a0b9d4
Cristian Gonzalez <cristian3629@gmail.com>
2020-10-29 22:34:53 -0300

    add more logs

--- src/socket_client.py
+++ src/socket_client.py
@@ -17,9 +17,11 @@ def create_socket(host, port):
 
     return sock
 
+
 def print_info_message(seq, response, quiet):
     if not quiet:
         print("{} bytes from 127.0.0.1: seq={} time={:.3f} ms".format(str(MSG_SIZE), seq, response))
+
 
 def print_packet_loss_message(seq, quiet):
     if not quiet:
@@ -30,27 +32,31 @@ def send_msg(socket, msg):
     socket.send(msg.encode())
 
 
-def send_ping_msg(socket, seq):
+def send_ping_msg(socket, seq, verbose):
+    if verbose:
+        print("Sending ping message")
     send_msg(socket, build_ping_msg(seq))
 
 
 def recv_msg(socket):
     return socket.recv(MSG_SIZE).decode()
 
-def wait_pong_msg(socket, seq):
+
+def wait_pong_msg(socket, seq, verbose):
     received_pong_msg = False
     while not received_pong_msg:
         msg = recv_msg(socket)
-        print("HOLA")
-        received_pong_msg = ( msg.startswith(PONG) and decode_pong_message(msg)[1] == seq )
+        if verbose:
+            print("Received message")
+        received_pong_msg = msg.startswith(PONG) and decode_pong_message(msg)[1] == seq
 
 
 # send ping message and wait pong message
 # return the rtt time in ms
-def send_ping(socket, seq):
+def send_ping(socket, seq, verbose):
     start = time.time()
-    send_ping_msg(socket, seq)
-    wait_pong_msg(socket, seq)
+    send_ping_msg(socket, seq, verbose)
+    wait_pong_msg(socket, seq, verbose)
     end = time.time()
     diff = end - start
     return float(diff * 1000)
