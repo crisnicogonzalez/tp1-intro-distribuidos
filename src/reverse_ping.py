a5478b1e8f4bc1c4812c60c7837320231ebf75dd
Cristian Gonzalez <cristian3629@gmail.com>
2020-10-29 22:45:34 -0300

    add more logs

--- src/reverse_ping.py
+++ src/reverse_ping.py
@@ -1,22 +1,18 @@
 from constants import MSG_SIZE, PING, TIMEOUT
 from payload_builder import build_reverse_msg, build_pong_msg, decode_ping_message, decode_rtt_message
-from socket_client import print_info_message, print_packet_loss_message
+from socket_client import print_info_message, print_packet_loss_message, send_msg, recv_msg
 import time
 
-def send_msg(socket, msg):
-    socket.send(msg.encode())
 
-
-def get_msg(socket):
-    msg = socket.recv(MSG_SIZE).decode()
-    return decode_rtt_message(msg)
-
-
-def wait_ping_msg(socket, seq):
+def wait_ping_msg(socket, seq, verbose):
+    if verbose:
+        print("Wait ping message")
     received_ping_msg = False
     while not received_ping_msg:
-        msg = get_msg(socket)
-        received_ping_msg = ( msg.startswith(PING) and decode_ping_message(msg)[1] == seq )
+        msg = recv_msg(socket)
+        received_ping_msg = msg.startswith(PING) and decode_ping_message(msg)[1] == seq
+    if verbose:
+        print("Ping message received")
 
 
 def send_pong_msg(socket, seq):
@@ -24,12 +20,13 @@ def send_pong_msg(socket, seq):
 
 
 def get_rtt_measure(socket, seq):
-    rtt = get_msg(socket)
+    rtt = recv_msg(socket)
     print(rtt)
     if rtt[1] == seq:
         return rtt[2]
     else:
         return rtt[1]
+
 
 def reverse_ping(socket, counts, quiet):
     measures = []
@@ -44,13 +41,14 @@ def reverse_ping(socket, counts, quiet):
 
     for seq in range(1, counts + 1):
         try:
-            wait_ping_msg(socket, seq)
+            wait_ping_msg(socket, seq, quiet)
             send_pong_msg(socket, seq)
             measure = float(get_rtt_measure(socket, seq))
             measures.append(measure)
             print_info_message(seq, measure, quiet)
 
         except Exception as e:
+            print(e)
             print_packet_loss_message(seq, quiet)
             time.sleep(1)
 
